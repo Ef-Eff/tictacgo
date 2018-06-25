370d1fba386dec770196609013f213297bb94bfc
Fabricio Ferreira <fabricio@mightysharp.co.uk>
2018-06-25 21:20:46 +0100

    moving stuff'

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Most of this code is based around the gorilla websocket chat example
-// This is just a learning exercise for me okay no booli
 package main
 
 import (
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # Hello, world
+
+Most of this code is based around the gorilla websocket chat example
+This is just a learning exercise for me okay no booli
